e77a503e5a3d17ac9a78b385bcb3af8f8aab7170
Aziz <kayumovabduaziz@gmail.com>
2022-09-15 12:42:36 +0900

    Simply Concatenation of Arrays
    - uses `vec::extend_from_within` instead of `for` loop.
    
    Closes: #3

--- src/concatenation_arrays.rs
+++ src/concatenation_arrays.rs
@@ -1,10 +1,7 @@
 // https://leetcode.com/problems/concatenation-of-array/
 impl Solution {
     pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len();
-        for i in 0..n {
-            nums.push(nums[i]);
-        }
-        return nums;
+        nums.extend_from_within(..);
+        nums
     }
 }
