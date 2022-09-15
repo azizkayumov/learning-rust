c3cd35ebde59393455ca494069a512102748ec47
Aziz <kayumovabduaziz@gmail.com>
2022-09-15 10:15:53 +0900

    added solution to Concatenation of Array

--- src/add_two_integers.rs
+++ src/add_two_integers.rs
@@ -1,5 +1,7 @@
+// https://leetcode.com/problems/add-two-integers/
+
 impl Solution {
     pub fn sum(num1: i32, num2: i32) -> i32 {
-        return num1 + num2;
+        return num1 + num2; // this does not overflow i32 because of the constraints: -100<= nums1,nums2 <= 100
     }
 }
