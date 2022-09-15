75a282d7797fb29f62072019423977726c7da655
Aziz <kayumovabduaziz@gmail.com>
2022-09-15 13:16:24 +0900

    Added unit test for Concatenation of Arrays
    - created a test function `test_get_concatenation` with `#[test]` attribute
    - included `mod concatenation_arrays` in `main.rs` so that `cargo test` detects the test
    
    Closes: #2

--- src/concatenation_arrays.rs
+++ src/concatenation_arrays.rs
@@ -1,7 +1,14 @@
 // https://leetcode.com/problems/concatenation-of-array/
-impl Solution {
-    pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
-        nums.extend_from_within(..);
-        nums
-    }
+
+pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+    nums.extend_from_within(..);
+    nums
+}
+
+#[test]
+fn test_get_concatenation() {
+    let input: Vec<i32> = [1, 2, 3, 4, 5].into();
+    let expected: Vec<i32> = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5].into();
+    let result = get_concatenation(input);
+    assert_eq!(expected, result);
 }
