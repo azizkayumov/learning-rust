77416eddbf3907f12b4324b654b72f61ba8b2860
Aziz <kayumovabduaziz@gmail.com>
2022-09-21 10:46:24 +0900

    added solution to Shuffle The Array

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod build_array_from_permutation;
 mod concatenation_arrays;
 mod richest_customer_wealth;
 mod running_sum_of_array;
+mod shuffle_the_array;
 
 fn main() {
     println!("Hello, world!");
