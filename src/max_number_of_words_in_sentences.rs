92ce7f585aa115c700fb67ddd27cabc44a6ff177
Aziz <kayumovabduaziz@gmail.com>
2022-09-22 10:58:21 +0900

    added solution to Max Number of Words Found in Sentences

--- src/max_number_of_words_in_sentences.rs
+++ src/max_number_of_words_in_sentences.rs
@@ -1,12 +1,19 @@
 // https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
 
 pub fn most_words_found(sentences: Vec<String>) -> i32 {
-    -1
+    // Assuming that a space char is not followed by another space char, and
+    // each sentences is trimmed (removed spaces from both sides), then:
+    // the number of words = the number of spaces + 1
+    sentences
+        .iter()
+        .map(|sentence| sentence.matches(" ").count() + 1)
+        .max()
+        .unwrap() as i32
 }
 
 #[test]
 fn test_most_words_founds() {
-    let expected = 3;
+    let expected = 6;
     let mut input: Vec<String> = Vec::new();
     input.push(String::from("alice and bob love leetcode"));
     input.push(String::from("i think so too"));
