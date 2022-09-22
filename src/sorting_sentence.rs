b56709e0789597e5dff695ef813b1387c441a9fd
Aziz <kayumovabduaziz@gmail.com>
2022-09-22 11:37:27 +0900

    added solution to Sorting the Sentences

--- src/sorting_sentence.rs
+++ src/sorting_sentence.rs
@@ -1,7 +1,24 @@
 // https://leetcode.com/problems/sorting-the-sentence/
 
+use std::ops::Add;
+
 pub fn sort_sentence(s: String) -> String {
-    String::from("-1")
+    // Split the input by a space, store it in a vector
+    let mut words: Vec<&str> = s.split(' ').collect();
+
+    // Sort the vector by the last char of each word
+    words.sort_by(|&a, &b| a.chars().last().unwrap().cmp(&b.chars().last().unwrap()));
+
+    let mut res = String::from("");
+    for i in 0..words.len() {
+        let word = words[i];
+        res += &word[0..(word.chars().count() - 1)];
+        if i != words.len() - 1 {
+            res += " ";
+        }
+    }
+
+    res
 }
 
 #[test]
