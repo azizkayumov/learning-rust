a56ec4f978ba35dd649eae36eb1ac07eb5ce50d6
Aziz <kayumovabduaziz@gmail.com>
2022-09-22 09:58:37 +0900

    added solution to Jewels and Stones

--- src/jewels_and_stones.rs
+++ src/jewels_and_stones.rs
@@ -1,7 +1,9 @@
 // https://leetcode.com/problems/jewels-and-stones/
 
 pub fn num_jewels_in_stones(jewels: String, stones: String) -> i32 {
-    -1
+    let mut result = 0;
+    result = stones.chars().filter(|&c| jewels.contains(c)).count();
+    result as i32
 }
 
 #[test]
@@ -9,6 +11,15 @@ fn test_with_no_jewels() {
     let expected = 0;
     let input_jewels = String::from("z");
     let input_stones = String::from("ZZ");
+    let output = num_jewels_in_stones(input_jewels, input_stones);
+    assert_eq!(expected, output);
+}
+
+#[test]
+fn test_with_jewels() {
+    let expected = 1;
+    let input_jewels = String::from("ngm");
+    let input_stones = String::from("kxg");
     let output = num_jewels_in_stones(input_jewels, input_stones);
     assert_eq!(expected, output);
 }
