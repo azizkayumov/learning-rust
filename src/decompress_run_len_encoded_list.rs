a7fcab370e8e660f1d16f27a60b7fa8071998d4e
Aziz <kayumovabduaziz@gmail.com>
2022-09-23 14:08:14 +0900

    added solution to Decompress Run-Length Encoded List

--- src/decompress_run_len_encoded_list.rs
+++ src/decompress_run_len_encoded_list.rs
@@ -1,7 +1,17 @@
 // https://leetcode.com/problems/decompress-run-length-encoded-list/
 
 pub fn decompress_rl_elist(nums: Vec<i32>) -> Vec<i32> {
-    [].into()
+    let mut res: Vec<i32> = Vec::new();
+    let mut i = 0;
+    while i < nums.len() {
+        let size = res.len();
+        let frequency = nums[i];
+        let value = nums[i + 1];
+        // Vec::resize() => it extends the vector if its new size is larger than prev.size and fills up with value:
+        res.resize(size + frequency as usize, value);
+        i += 2;
+    }
+    res
 }
 
 #[test]
